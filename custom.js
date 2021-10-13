bf0417a2f6807d214aa74108b2bab11bb5c2023a
NAYEEM65 <mathnayeem054@gmail.com>
2021-10-14 00:25:05 +0600

    code updated

---
+++ custom.js
@@ -0,0 +1,50 @@
+// var x = 35;
+// if(x%5==0){
+//     console.log("the number is divided by 5");
+//     console.log(++x)
+// }
+// else{
+//     console.log("errr");
+//     console.log(--x);
+// }
+
+// const num1 = 3;
+// const num2 = 4;
+// console.log(num1+num2);
+
+
+// const Emon = function(num1, num2) {
+//     sum = num1 + num2;
+//     console.log(sum);
+// }
+
+// function Emon(num1, num2) {
+//     sum = num1 + num2;
+//     console.log(sum);
+// }
+
+// Emon(3, 4)
+// Emon(4, 4)
+// Emon(3, 7)
+// Emon(87, 4)
+// Emon(13, 3)
+// Emon(13, 3)
+
+
+
+
+// const area = function (r){
+//     return(3.1416*r*r)
+// }
+// const getArea = area(5)
+// console.log(getArea);
+
+
+const myObject = {
+    bangladesh: "cruption",
+    america: "technology",
+    india: "cow's pee",
+}
+
+console.log(myObject.bangladesh+ ' ' + myObject.america);
+console.log(myObject.india);
